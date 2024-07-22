8239d4775d5e05c7e235b9c36cc2db85b3c851a0
Teodor469 <lifetroll6@gmail.com>
2024-07-22 14:08:23 +0300

    .

---
+++ todo-app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import ToDoList from './ToDoList'
+
+function App() {
+  return(
+    <>
+      <ToDoList/>
+    </>
+  )
+}
+
+export default App
